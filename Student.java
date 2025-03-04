702b06967ba8b68a42f2ac1f32bd937db4b49569
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-04 23:34:08 +0530

    Redesign the Getter&Settters in Student.java

--- Student.java
+++ Student.java
@@ -1,68 +1,36 @@
-//Student.java
+// Student.java
+class Student {
+    private String name;
+    private int prn;
+    private String dept;
+    private String batch;
+    private float cgpa;
 
-class Student{
-	private String name;
-	private int prn;
-	private String dept;
-	private String batch;
-	private float cgpa;
+    public Student(String name, int prn, String dept, String batch, float cgpa) {
+        this.name = name;
+        this.prn = prn;
+        this.dept = dept;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
 
-	public Student(String name, int prn, String dept,String batch, float cgpa){
-		this.name = name;
-		this.prn = prn;
-		this.dept = dept;
-		this.batch = batch;
-		this.cgpa = cgpa;
-	}
+    // Getters and Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-	//Name
-	public String getName(){
-		return name;
-	}
+    public int getPRN() { return prn; }
+    public void setPRN(int prn) { this.prn = prn; }
 
-	public void setName(String name){
-		this.name = name;
-	}
+    public String getDept() { return dept; }
+    public void setDept(String dept) { this.dept = dept; }
 
-	//PRN
-	public int getPRN(){
-		return prn;
-	}
+    public String getBatch() { return batch; }
+    public void setBatch(String batch) { this.batch = batch; }
 
-	public void setPRN(int prn){
-		this.prn = prn;
-	}
+    public float getCGPA() { return cgpa; }
+    public void setCGPA(float cgpa) { this.cgpa = cgpa; }
 
-	//Dept
-	public String getDept(){
-		return dept;
-	}
-
-	public void setDept(String dept){
-		this.dept = dept;
-	}
-
-	//Batch
-	public String getBatch(){
-		return batch;
-	}
-
-	public void setBatch(String batch){
-		this.batch = batch;
-	}
-
-	//CGPA
-	public float getCGPA(){
-		return cgpa;
-	}
-
-	public void setCGPA(float cgpa){
-		this.cgpa = cgpa;
-	}
-
-	//Display
-	public void display(){
-		System.out.println("Name: "+ name + " PRN: " + prn + " Dept: " + dept + " Batch: " + batch + " CGPA: " + cgpa);
-	}
-
+    public void display() {
+        System.out.println("Name: " + name + " | PRN: " + prn + " | Dept: " + dept + " | Batch: " + batch + " | CGPA: " + cgpa);
+    }
 }
